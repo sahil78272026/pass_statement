687d7b07ef82ce0e50066e1a75c255fb86613d70
sahil78272026 <sahil.garg78272026@gmail.com>
2022-05-05 21:53:20 +0530

    Add files via upload

---
+++ 09_pass_statement.py
@@ -0,0 +1,11 @@
+i = 10
+if i<4:
+    pass # will pass the program and do nothing 
+
+def sum(player):
+    pass # will pass the program and do nothing , provide null value
+
+def ouch(sound):
+    pass # will pass the program and do nothing , provide null value
+
+print (i)
